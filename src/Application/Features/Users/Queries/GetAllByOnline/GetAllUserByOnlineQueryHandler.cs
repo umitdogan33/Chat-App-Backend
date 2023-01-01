4c1d3bf1681ddaa298dec7219acdaf56e6ea4d4f
umitdogan33 <umit9551@gmail.com>
2023-01-01 12:31:03 +0300

    Refactor

--- src/Application/Features/Users/Queries/GetAllByOnline/GetAllUserByOnlineQueryHandler.cs
+++ src/Application/Features/Users/Queries/GetAllByOnline/GetAllUserByOnlineQueryHandler.cs
@@ -1,10 +1,12 @@
-﻿using Application.Common.Hubs;
+﻿using System.Security.Claims;
+using Application.Common.Hubs;
 using Application.Features.Auth.Dtos;
 using Application.Features.Auth.Models;
 using Application.Repositories.EntityFramework;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Users.Queries.GetAllByOnline;
@@ -14,17 +16,19 @@ public class GetAllUserByOnlineQueryHandler : IRequestHandler<GetAllUserByOnline
     public List<User> Users { get; set; }
     private IMapper _mapper;
     private IUserRepository _userRepository;
+    private IHttpContextAccessor _httpContextAccessor;
 
-    public GetAllUserByOnlineQueryHandler(IMapper mapper, IUserRepository userRepository)
+    public GetAllUserByOnlineQueryHandler(IMapper mapper, IUserRepository userRepository,IHttpContextAccessor httpContextAccessor)
     {
         _mapper = mapper;
         _userRepository = userRepository;
         Users = new();
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<IList<UserListByOnline>> Handle(GetAllUserByOnlineQuery request, CancellationToken cancellationToken)
     {
-        var rowData = ConnectedUser.ClientsData.FindAll(p => p.Status == true).ToList();
+        var rowData = ConnectedUser.ClientsData.FindAll(p => p.Status == true && p.UserId != _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value).ToList();
         foreach (var item in rowData)
         {
             var data = await _userRepository.GetAsync(p => p.Id == item.UserId);
--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -40,7 +40,7 @@ if (app.Environment.IsDevelopment())
 }
 
 if (app.Environment.IsProduction())
-    //app.ConfigureCustomExceptionMiddleware();
+    app.ConfigureCustomExceptionMiddleware();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
